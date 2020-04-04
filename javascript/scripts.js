0b21a426af63c8ead623d71f2e75cca24237c2d1
Vinícius Dias <vinixiussouza@gmail.com>
2020-04-04 13:37:40 -0300

    Criação do front-end

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,8 +1,16 @@
-document
-    .querySelector("button.fat")
-    .addEventListener("click", function(){
-        document
-            .querySelector("#footer")
-            .classList
-            .toggle("hide")
-    })
+function onOff(){ /* funcionalidade - ouvidor do evento de click */
+    document
+        .querySelector("#modal")
+        .classList /* propriedade */
+        .toggle("hide") /* colocar e tirar o hide */
+
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")
+
+    document 
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")
+}
